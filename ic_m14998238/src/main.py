e7b66d9361e92a84551b6c7fb50bb615e7c6d290
Evan Heidenreich <Heidenen@mail.uc.edu>
2026-09-11 11:19:56 -0400

    Completed Task 1 and Started Task 2 of Encryption and Decryption

---
+++ ic_m14998238/src/main.py
@@ -0,0 +1,20 @@
+def text_count(text):
+    count = len(text)
+    print(count)
+    return count
+
+
+def frequency(text):
+    pass
+
+
+def index_of_coincidence(text):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
